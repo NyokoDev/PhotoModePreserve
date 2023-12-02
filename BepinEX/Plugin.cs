fc5034ada17f200aa2ee76071a70bed555c0bda7
NyokoDev <129089982+NyokoDev@users.noreply.github.com>
2023-12-01 18:51:18 -0600

    Add project files.

---
+++ BepinEX/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+#if BEPINEX_V6
+    using BepInEx.Unity.Mono;
+#endif
+namespace DynamicResolutionExtended
+{
+    using System.Reflection;
+    using BepInEx;
+    using DynamicResolutionExtended.Systems;
+    using Game;
+    using Game.Common;
+    using Game.Rendering;
+    using Game.SceneFlow;
+    using Game.UI.InGame;
+    using HarmonyLib;
+
+    [BepInPlugin(GUID, "Photo Mode Preserve", "1.0")]
+    [HarmonyPatch]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "com.nyoko.photomodepreserve";
+        private Mod _mod;
+   
+
+        public void Awake()
+        {
+   
+            _mod = new();
+            _mod.OnLoad();
+
+            _mod.Log.Info("=======PHOTO MODE PRESERVE=======");
+            _mod.Log.Info("=======     Initialized   =======");
+            _mod.Log.Info("=======                   =======");
+           
+            // Apply Harmony patches.
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+        }
+
+        /// <summary>
+        /// Harmony postfix to <see cref="SystemOrder.Initialize"/> to substitute for IMod.OnCreateWorld.
+        /// </summary>
+        /// <param name="updateSystem"><see cref="GameManager"/> <see cref="UpdateSystem"/> instance.</param>
+        [HarmonyPatch(typeof(SystemOrder), nameof(SystemOrder.Initialize))]
+        [HarmonyPostfix]
+        private static void InjectSystems(UpdateSystem updateSystem) => Mod.Instance.OnCreateWorld(updateSystem);
+
+        [HarmonyPatch(typeof(PhotoModeRenderSystem), nameof(PhotoModeRenderSystem.DisableAllCameraProperties))]
+        [HarmonyPostfix]
+        public static void ActivatePatch()
+        {
+            DyUISystem.Instance.DisableAllCameraProperties();
+        }
+   
+    }
+}
+// Code authored by Nyoko
+// Feel free to reach out for any questions or clarifications!
+// Huge thanks to Algernon for input on mod structure.
---
+++ Mod.cs
@@ -0,0 +1,53 @@
+﻿namespace DynamicResolutionExtended
+{
+
+    using Colossal.Logging;
+    using Game;
+    using Game.Modding;
+    using DynamicResolutionExtended.Systems;
+
+    public sealed class Mod : IMod
+    {
+
+        // - Start of mod properties.
+        public const string ModName = "Photo Mode Preserve";                    
+        public static Mod Instance { get; private set; }
+        internal ILog Log { get; private set; }
+        public void OnLoad()
+        {
+            Instance = this;
+            Log = LogManager.GetLogger(ModName);
+            Log.Info("setting logging level to Debug");
+            Log.effectivenessLevel = Level.Debug;
+
+            Log.Info("loading");
+            Log.Info("=======PHOTO MODE PRESERVE=======");
+            Log.Info("=======     Initialized   =======");
+            Log.Info("=======                   =======");
+        }
+
+        //End of mod properties.
+
+        /// <summary>
+        /// Called by the game when the game world is created. 
+        /// </summary>
+        /// <param name="updateSystem">Game update system.</param>
+        public void OnCreateWorld(UpdateSystem updateSystem)
+        {
+            Log.Info("[DYNRESEXT] starting OnCreateWorld");
+
+   
+ 
+
+        }
+
+        /// <summary>
+        /// Called by the game when the mod is disposed of.
+        /// </summary>
+        public void OnDispose()
+        {
+            Log.Info("disposing");
+            Instance = null;
+        }
+    }
+}
